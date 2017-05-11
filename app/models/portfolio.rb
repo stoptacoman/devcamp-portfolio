39c3359bd3f22c3f5348a8dfce9956742793f9bc
stoptacoman <walsh.dc@gmail.com>
2017-05-11 13:37:19 -0400

    Implemented data defaults on portfolios model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,4 +4,11 @@ class Portfolio < ApplicationRecord
   def self.another_service
     where(subtitle: 'Another service')
   end
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 end
