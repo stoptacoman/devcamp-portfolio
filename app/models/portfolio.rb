c17654c9e255ac7bfbea4e68b2074e24565f2db4
stoptacoman <walsh.dc@gmail.com>
2017-05-11 13:23:58 -0400

    Implemented custom scope for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,7 @@
 class Portfolio < ApplicationRecord
-  validates_presence_of :title, :body, :main_image, :thumbnail_image
+  validates_presence_of :title, :body, :main_image, :thumb_image
+
+  def self.another_service
+    where(subtitle: 'Another service')
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,19 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "Blog post number #{blog}",
     body: "Ex nisi distillery etsy, poke neutra eiusmod intelligentsia bicycle rights
-          aesthetic jianbing iceland pop-up marfa. Disrupt flannel cillum fam, keytar 
-          drinking vinegar activated charcoal brunch sint nisi pabst. Green juice 
-          nulla heirloom messenger bag."
+          aesthetic jianbing iceland pop-up marfa. Disrupt flannel cillum fam, keytar
+          drinking vinegar activated charcoal brunch sint nisi pabst. Green juice
+          nulla heirloom messenger bag.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -15,7 +24,7 @@ puts "10 blog posts created"
     title: "Rails #{skill}",
     percent_utilized: 15
   )
-  
+
 end
 
 puts "5 skills created"
@@ -24,8 +33,8 @@ puts "5 skills created"
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "Some service",
-    body: "Yr hot chicken helvetica, air plant humblebrag actually twee la croix 
-          pok pok waistcoat kogi chicharrones. Eu non next level, marfa laborum 
+    body: "Yr hot chicken helvetica, air plant humblebrag actually twee la croix
+          pok pok waistcoat kogi chicharrones. Eu non next level, marfa laborum
           farm-to-table occupy consequat put a bird on it.",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200"
